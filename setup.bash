3af7c33b8fb1fd49661ecae583ddcde8767177e5
Tianjian Chen <tianjian.chen@columbia.edu>
2020-11-05 14:15:00 -0500

    removed old setup.bash

--- setup.bash
+++
@@ -1,6 +0,0 @@
-# This file needs to be at the root of project dir
-rl_env #defined in my.bashrc
-export PROJECTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-export PYTHONPATH=${PROJECTDIR}
-source ${PROJECTDIR}/venv_mass_spring/bin/activate
-echo "Mass-spring toy problem environment ready."
